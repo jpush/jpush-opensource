68ca2ee3d14da1c680693deb08eace102704d5f3
fendouai <fendouai@gmail.com>
2016-04-27 11:27:06 +0800

    Responsive layout

--- js/public.js
+++ js/public.js
@@ -1,38 +1,18 @@
-var xmlhttp;
-function loadXMLDoc(url)
-{
-xmlhttp=null;
-if (window.XMLHttpRequest)
-  {// code for IE7, Firefox, Opera, etc.
-  xmlhttp=new XMLHttpRequest();
-  }
-else if (window.ActiveXObject)
-  {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-if (xmlhttp!=null)
-  {
-  xmlhttp.onreadystatechange=state_Change;
-  xmlhttp.open("GET",url,true);
-  xmlhttp.send(null);
-  }
-else
-  {
-  alert("Your browser does not support XMLHTTP.");
-  }
-}
 
-function state_Change()
-{
-if (xmlhttp.readyState==4)
-  {// 4 = "loaded"
-  if (xmlhttp.status==200)
-    {// 200 = "OK"
-    var obj= eval ("(" + xmlhttp.responseText + ")");
 
+$.get("https://api.github.com/orgs/jpush/repos",function(data,status){
+    data.sort(function(y,x){
+        return x["updated_at"].localeCompare(y["updated_at"]);
+        });
+    var obj= eval(data);
     var repos=document.getElementById("repos");
     for (var i=0; i < obj.length;i++) {
           var ul=document.createElement("ul"); 
+          ul.setAttribute("class","col-md-4 col-sm-6 col-xs-12"); 
+
+          var ul_div=document.createElement("div");
+          ul_div.setAttribute("class","ul_div"); 
+          ul.appendChild(ul_div);
 
           var li_name_a=document.createElement("a");
           li_name_a.setAttribute("href", obj[i].html_url); 
@@ -44,29 +24,27 @@ if (xmlhttp.readyState==4)
           li_name.appendChild(li_name_a);
 
           li_name.setAttribute("class", "name"); 
-          ul.appendChild(li_name);
+          ul_div.appendChild(li_name);
 
           var li_language=document.createElement("li");
           var language=document.createTextNode(obj[i].language);
           li_language.appendChild(language);  
           li_language.setAttribute("class", "language");     
-          ul.appendChild(li_language);
+          ul_div.appendChild(li_language);
 
           var li_description=document.createElement("li");
           var description=document.createTextNode(obj[i].description);
           li_description.appendChild(description); 
-          li_description.setAttribute("class", "description");        
-          ul.appendChild(li_description);
+          li_description.setAttribute("class", "description");  
+
+          ul_div.appendChild(li_description);
 
           repos.appendChild(ul);
-        };
+        }
     }
-  else
-    {
-    alert("Problem retrieving XML data:" + xmlhttp.statusText);
-    }
-  }
-}
+);
+
+
 
 $.get("https://api.github.com/orgs/jpush/repos?page=1&per_page=100",function(data,status){
     $("#num-repos").text(data.length);
